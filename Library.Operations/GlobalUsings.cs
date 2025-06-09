c8edab70bda4a093437896322ad60d9e57f901ae
Will Bellman <wbellman@gmail.com>
2025-06-09 13:37:32 -0500

    Added comment.

--- Library.Operations/GlobalUsings.cs
+++ Library.Operations/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using System;
+﻿// These are the Global Usings for the Library.Operations project.
+global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Runtime.CompilerServices;
